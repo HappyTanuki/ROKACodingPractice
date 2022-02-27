ac006330ac10fcf9856a7b89806bf528ac70a873
Guest <guest-kak8pp@h26610.sgb.skir.kr>
2022-02-27 22:22:37 +0900

    중간 저장

--- DatastructureWrittenInEasyWay/Chapter02/exercise_18_C.c
+++ DatastructureWrittenInEasyWay/Chapter02/exercise_18_C.c
@@ -1,29 +1,40 @@
 int ackermann(int m, int n){
-    int stack[100] = {0, }, stackTop = -1;
+    int stack[3][100] = {0, }, stackTop = -1;
 
-    stack[++stackTop] = m;
+    stack[0][++stackTop] = m;
+    stack[1][stackTop] = n;
+    stack[2][stackTop] = -1;
     
     while (stackTop != -1){
-        m = stack[stackTop--];
+        m = stack[0][stackTop--];
+        n = stack[1][stackTop + 1];
 
         if (m == 0){
-            stack[++stackTop] = n + 1;
+            stack[0][++stackTop] = m;
+            stack[1][stackTop] = n;
+            stack[2][stackTop] = n + 1;
             if (stackTop == 0){
                 stackTop = -1;
             }
             continue;
         }
         if (n == 0){
-            stack[++stackTop] = m - 1;
-            n = 1;
+            stack[0][++stackTop] = m - 1;
+            stack[1][stackTop] = 1;
+            stack[2][stackTop] = -1;
             continue;
         }
         if (m >= 1 && n >= 1){
-            stack[++stackTop] = m - 2;
-            stack[++stackTop] = m;
-            n -= 1;
+            stack[0][++stackTop] = m - 2;
+            stack[1][stackTop] = -1;
+            stack[2][stackTop] = -1;
+            stack[0][++stackTop] = m;
+            stack[1][stackTop] = n - 1;
+            stack[2][stackTop] = -1;
+            continue;
         }
+        break;
     }
 
-    return stack[0];
+    return stack[2][0];
 }
--- DatastructureWrittenInEasyWay/Chapter02/main.c
+++ DatastructureWrittenInEasyWay/Chapter02/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "exercise_18.c"
+#include "exercise_18_C.c"
 
 int main(){
     printf("%d\n", ackermann(2, 3));
