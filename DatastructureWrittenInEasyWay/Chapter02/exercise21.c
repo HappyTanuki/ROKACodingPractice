14c0edcf4a1ce594e6e36cccf65fbd673ae921f2
Guest <guest-unc3ay@h26607.sgb.skir.kr>
2022-03-01 22:22:13 +0900

    Chapter02 학습 완료

--- DatastructureWrittenInEasyWay/Chapter02/exercise21.c
+++ DatastructureWrittenInEasyWay/Chapter02/exercise21.c
@@ -23,3 +23,46 @@ void flood_fill(int x, int y){
         flood_fill(x + 1, y);
     }
 }
+
+/**
+#include <stdio.h>
+#include <time.h>
+#include "exercise21.c"
+
+int main(){
+    
+    for (int j = 0; j < 10; j++){
+        for (int i = 0; i < 10; i++){
+            if ((i == 2 && j >= 3 && j <= 6) ||
+                ((i == 3 || i == 4) && j >= 4 && j <= 6) ||
+                (i == 5 && j >= 4 && j <= 7) ||
+                (i >= 6 && i <= 8 && j == 4)){
+                write_pixel(i, j, WHITE);
+            }
+            else {
+                write_pixel(i, j, YELLOW);
+            }
+        }
+        
+    }
+    clock_t start = clock(), stop;
+    printf("영역 채우기 전:\n");
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j < 10; j++){
+            printf("%d\t", read_pixel(i, j));
+        }
+        printf("\n");
+    }
+    flood_fill(5, 4);
+    printf("영역 채우기 후:\n");
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j < 10; j++){
+            printf("%d\t", read_pixel(i, j));
+        }
+        printf("\n");
+    }
+    stop = clock();
+    printf("\n수행시간:%lf\n", (double)((stop - start) / (double)CLOCKS_PER_SEC));
+    return 0;
+}
+**/
--- DatastructureWrittenInEasyWay/Chapter02/main.c
+++ DatastructureWrittenInEasyWay/Chapter02/main.c
@@ -4,22 +4,36 @@
 
 int main(){
     
-    for (int i = 0; i < 10; i++){
-            for (int j = 0; j < 10; j++){
-                if (j >= 3 && j <= 7 && i >= 2 && i <= 8){
-                    if ((i == 3 && j <= 6) || j == 4 || ((j == 5 || j == 6) && i <= 5) || (i == 6 && j == 5)){ // 아직 미완
-                        write_pixel(i, j, WHITE);
-                    }
-                }
-                else {
-                    write_pixel(i, j, YELLOW);
-                }
+    for (int j = 0; j < 10; j++){
+        for (int i = 0; i < 10; i++){
+            if ((i == 2 && j >= 3 && j <= 6) ||
+                ((i == 3 || i == 4) && j >= 4 && j <= 6) ||
+                (i == 5 && j >= 4 && j <= 7) ||
+                (i >= 6 && i <= 8 && j == 4)){
+                write_pixel(i, j, WHITE);
             }
+            else {
+                write_pixel(i, j, YELLOW);
+            }
+        }
         
     }
-
     clock_t start = clock(), stop;
-
+    printf("영역 채우기 전:\n");
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j < 10; j++){
+            printf("%d\t", read_pixel(i, j));
+        }
+        printf("\n");
+    }
+    flood_fill(5, 4);
+    printf("영역 채우기 후:\n");
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j < 10; j++){
+            printf("%d\t", read_pixel(i, j));
+        }
+        printf("\n");
+    }
     stop = clock();
     printf("\n수행시간:%lf\n", (double)((stop - start) / (double)CLOCKS_PER_SEC));
     return 0;
